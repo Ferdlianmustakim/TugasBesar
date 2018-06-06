ef7674274a7847de7d3c991d11eff228d3e654d3
Imam3997 <39964516+Imam3997@users.noreply.github.com>
2018-06-06 15:31:30 +0700

    TUGAS
    SELESAI.

---
+++ Laporan Tugas Besar/RIANok/RIANok/Form2.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace RIANok
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.nama = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tglbeli = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nanas = new System.Windows.Forms.TextBox();
+            this.anggur = new System.Windows.Forms.TextBox();
+            this.stroberi = new System.Windows.Forms.TextBox();
+            this.kopi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.totalbiaya = new System.Windows.Forms.TextBox();
+            this.kembalian = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.uang_byr = new System.Windows.Forms.TextBox();
+            this.biayapajak = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VAPOR STORE";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Location = new System.Drawing.Point(37, 57);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "NAMA";
+            // 
+            // nama
+            // 
+            this.nama.Location = new System.Drawing.Point(166, 57);
+            this.nama.Name = "nama";
+            this.nama.Size = new System.Drawing.Size(127, 20);
+            this.nama.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label11.Location = new System.Drawing.Point(37, 86);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(84, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "TANGGAL BELI";
+            // 
+            // tglbeli
+            // 
+            this.tglbeli.Location = new System.Drawing.Point(166, 83);
+            this.tglbeli.Name = "tglbeli";
+            this.tglbeli.Size = new System.Drawing.Size(127, 20);
+            this.tglbeli.TabIndex = 23;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(296, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 30);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "TAMPIL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(404, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(188, 30);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "HAPUS";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(555, 308);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(129, 30);
+            this.button6.TabIndex = 26;
+            this.button6.Text = "KEMBALI";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(37, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NANAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(37, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ANGGUR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(37, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "STROBERI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(37, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "KOPI";
+            // 
+            // nanas
+            // 
+            this.nanas.Location = new System.Drawing.Point(165, 109);
+            this.nanas.Name = "nanas";
+            this.nanas.Size = new System.Drawing.Size(127, 20);
+            this.nanas.TabIndex = 5;
+            // 
+            // anggur
+            // 
+            this.anggur.Location = new System.Drawing.Point(165, 135);
+            this.anggur.Name = "anggur";
+            this.anggur.Size = new System.Drawing.Size(127, 20);
+            this.anggur.TabIndex = 6;
+            // 
+            // stroberi
+            // 
+            this.stroberi.Location = new System.Drawing.Point(165, 161);
+            this.stroberi.Name = "stroberi";
+            this.stroberi.Size = new System.Drawing.Size(127, 20);
+            this.stroberi.TabIndex = 7;
+            // 
+            // kopi
+            // 
+            this.kopi.Location = new System.Drawing.Point(166, 187);
+            this.kopi.Name = "kopi";
+            this.kopi.Size = new System.Drawing.Size(127, 20);
+            this.kopi.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "TOTAL BIAYA";
+            // 
+            // totalbiaya
+            // 
+            this.totalbiaya.Location = new System.Drawing.Point(165, 233);
+            this.totalbiaya.Name = "totalbiaya";
+            this.totalbiaya.Size = new System.Drawing.Size(127, 20);
+            this.totalbiaya.TabIndex = 10;
+            // 
+            // kembalian
+            // 
+            this.kembalian.Location = new System.Drawing.Point(404, 314);
+            this.kembalian.Name = "kembalian";
+            this.kembalian.Size = new System.Drawing.Size(127, 20);
+            this.kembalian.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(314, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "KEMBALIAN";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(37, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "BAYAR";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Location = new System.Drawing.Point(37, 317);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "BIAYA PAJAK 10%";
+            // 
+            // uang_byr
+            // 
+            this.uang_byr.Location = new System.Drawing.Point(165, 292);
+            this.uang_byr.Name = "uang_byr";
+            this.uang_byr.Size = new System.Drawing.Size(127, 20);
+            this.uang_byr.TabIndex = 15;
+            // 
+            // biayapajak
+            // 
+            this.biayapajak.Location = new System.Drawing.Point(166, 317);
+            this.biayapajak.Name = "biayapajak";
+            this.biayapajak.Size = new System.Drawing.Size(127, 20);
+            this.biayapajak.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 30);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "hitung";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(491, 30);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "total";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 30);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(598, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 30);
+            this.button7.TabIndex = 27;
+            this.button7.Text = "TAMBAH";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // dg1
+            // 
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Location = new System.Drawing.Point(317, 64);
+            this.dg1.Name = "dg1";
+            this.dg1.Size = new System.Drawing.Size(384, 217);
+            this.dg1.TabIndex = 28;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(820, 369);
+            this.Controls.Add(this.dg1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.tglbeli);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.nama);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.biayapajak);
+            this.Controls.Add(this.uang_byr);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.kembalian);
+            this.Controls.Add(this.totalbiaya);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.kopi);
+            this.Controls.Add(this.stroberi);
+            this.Controls.Add(this.anggur);
+            this.Controls.Add(this.nanas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox nama;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tglbeli;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nanas;
+        private System.Windows.Forms.TextBox anggur;
+        private System.Windows.Forms.TextBox stroberi;
+        private System.Windows.Forms.TextBox kopi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox totalbiaya;
+        private System.Windows.Forms.TextBox kembalian;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox uang_byr;
+        private System.Windows.Forms.TextBox biayapajak;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridView dg1;
+    }
+}
---
+++ Laporan Tugas Besar/RIANok/RIANok/Form2.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RIANok
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+
+            InitializeComponent();
+        }
+        SqlConnection Conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        string qry;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Conn.ConnectionString = "Data Source = DESKTOP-J1DNFS8\\SQLEXPRESS; Initial Catalog=Vapor; Integrated Security=True";
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double bpnanas, bpanggur, bpstroberi, bpkopi, bptotalbiaya;
+            double hargaNn, hargaAg, hargaSt, hargaKp, bptotalpembayaran;
+
+
+            bpnanas = double.Parse(nanas.Text);
+            bpanggur = double.Parse(anggur.Text);
+            bpstroberi = double.Parse(stroberi.Text);
+            bpkopi = double.Parse(kopi.Text);
+
+
+            hargaNn = bpnanas * 50000;
+            hargaAg = bpanggur * 60000;
+            hargaSt = bpstroberi * 70000;
+            hargaKp = bpkopi * 80000;
+
+            bptotalbiaya = hargaNn + hargaAg + hargaSt + hargaKp;
+
+
+            totalbiaya.Text = "Rp." + bptotalbiaya.ToString("N");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double bpnanas, bpanggur, bpstroberi, bpkopi, bptotalbiaya, bpuang_byr, bpbiayapajak, bpkembalian;
+            double hargaNn, hargaAg, hargaSt, hargaKp, bptotalpembayaran;
+
+
+            bpnanas = double.Parse(nanas.Text);
+            bpanggur = double.Parse(anggur.Text);
+            bpstroberi = double.Parse(stroberi.Text);
+            bpkopi = double.Parse(kopi.Text);
+            bpuang_byr = double.Parse(uang_byr.Text);
+
+
+            hargaNn = bpnanas * 50000;
+            hargaAg = bpanggur * 60000;
+            hargaSt = bpstroberi * 70000;
+            hargaKp = bpkopi * 80000;
+
+            bptotalbiaya = hargaNn + hargaAg + hargaSt + hargaKp;
+            bpbiayapajak = bptotalbiaya * 0.1;
+
+            bptotalpembayaran = bptotalbiaya + bpbiayapajak;
+            bpkembalian = bpuang_byr - bptotalpembayaran;
+
+            totalbiaya.Text = "Rp." + bptotalbiaya.ToString("N");
+            biayapajak.Text = "Rp." + bpbiayapajak.ToString("N");
+            totalbiaya.Text = "Rp." + bptotalpembayaran.ToString("N");
+            kembalian.Text = "Rp." + bpkembalian.ToString("N");
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                qry = "SELECT*FROM tb_vapor";
+                cmd = new SqlCommand();
+                cmd.Connection = Conn;
+                cmd.CommandText = qry;
+                da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                dt = new DataTable();
+                da.Fill(dt);
+                dg1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                da.Dispose();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                qry = "DELETE FROM tb_vapor WHERE NAMA = " + dg1.CurrentRow.Cells["NAMA"].FormattedValue;
+                cmd.Connection = Conn;
+                cmd.CommandText = qry;
+                MessageBox.Show("Berhasil Disimpan");
+                Conn.Open();
+                cmd.ExecuteNonQuery();
+                button4_Click(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                qry = "INSERT INTO tb_vapor (NAMA,TANGGAL,TOTAL BIAYA,NANAS,ANGGUR,STROBERI,KOPI,BAYAR,BIAYA PAJAK,KEMBALIAN)VALUES('" + nama.Text + "','" + tglbeli.Text + "','"
+                + totalbiaya.Text + "','" + nanas.Text + "','" + anggur.Text + "','" + stroberi.Text + "','" + kopi.Text + "','" + uang_byr.Text + "','" + biayapajak.Text + "','" + kembalian.Text + "')";
+                cmd.Connection = Conn;
+                cmd.CommandText = qry;
+                MessageBox.Show("Berhasil Disimpan");
+                Conn.Open();
+                cmd.ExecuteNonQuery();
+                button4_Click(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Login X = new Login();
+            X.Show();
+            this.Hide();
+        }
+    }
+}
